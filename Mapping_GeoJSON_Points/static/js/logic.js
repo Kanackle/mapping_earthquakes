49c716f8a674b523d6d2ab6d2391dc56c93dd270
Kanackle <Kanak.Somani02@myhunter.cuny.edu>
2022-12-19 23:56:55 -0500

    Adding GeoJSON markers from a URL

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,59 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at San Francisco airport
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"14",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-/*
-//Grabbing our GeoJSON data
-L.geoJSON(sanFranAirport, {
-    //Turn each feature into a marker on the map
-    pointToLayer: function(feature, latlng){
-        console.log(feature);
-        return L.marker(latlng)
-        .bindPopup("<h2>" + feature.properties.name + "</br>" + feature.properties.city + "</h2>");
-    }
-}).addTo(map);
-*/
-
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer){
-        console.log(layer);
-        layer.bindPopup("<h2>" + feature.properties.name + "</br>" + feature.properties.city + "</h2>");
-    }
-}).addTo(map);
-
-//Getting data from cities.js
-//let cityData = cities;
-
-//Loop through the cities array and create one marker for each city
-/*cityData.forEach(function(city){
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});*/
+// Create the map object with center and zoom level
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -64,3 +13,20 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map); 
+
+//Accessing airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Kanackle/mapping_earthquakes/main/majorAirports.json"
+
+//Grabbing GeoJSON data
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    console.log(data.properties)
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    pointToLayer: function(feature, latlng){
+        return L.marker(latlng)
+        .bindPopup("<h2>Airport Code</h2>" + "<h3>" + feature.properties.faa + "</h3></br>" + "<h2>Airport Name</h2>" + "<h3>" + feature.properties.name + "</h3>")
+    }
+  }).addTo(map);
+});
